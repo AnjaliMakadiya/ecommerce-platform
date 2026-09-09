40c056207235f901200231b9481a87d8e1483d9a
Anjali Makadiya <anjalimakadiya25@gmail.com>
2026-09-09 13:06:23 +0530

    Configure Keycloak hostname for reverse proxy

--- ecommerce-dashboard/src/auth/keycloak.js
+++ ecommerce-dashboard/src/auth/keycloak.js
@@ -1,7 +1,8 @@
 import Keycloak from 'keycloak-js';
 
 const keycloak = new Keycloak({
-  url: 'http://localhost:8181',
+  // url: 'http://localhost:8181',
+  url: 'https://anjali-ecommerce.duckdns.org/auth',
   realm: 'ecommerce-realm',
   clientId: 'ecommerce-frontend',
 });
